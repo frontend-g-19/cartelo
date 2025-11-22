b0ad9d652c425b8a2913a90460738d344cfd26bf
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-11-22 19:36:08 +0500

    created and added several new products

---
+++ src/db/data.js
@@ -0,0 +1,141 @@
+export const defaultProducts = [
+  {
+    id: 21,
+    title: "HP Pavilion Gaming Laptop",
+    price: 799.99,
+    description:
+      "Powerful gaming laptop with Ryzen processor and GTX graphics. Ideal for both gaming and work.",
+    category: "electronics",
+    image:
+      "https://assets.asaxiy.uz/product/items/desktop/dc5c7986daef50c1e02ab09b442ee34f2021072214530717265drDQpsUdfW.jpg.webp",
+    rating: {
+      rate: 4.4,
+      count: 520,
+    },
+  },
+  {
+    id: 22,
+    title: "Timberland Classic Boots",
+    price: 139.99,
+    description:
+      "Durable waterproof boots designed for hiking and outdoor adventures.",
+    category: "men's shoes",
+    image:
+      "https://gemplers.com/cdn/shop/products/233920WHT.2_800x800.jpg?v=1707502554",
+    rating: {
+      rate: 4.6,
+      count: 310,
+    },
+  },
+  {
+    id: 23,
+    title: "Ray-Ban Aviator Sunglasses",
+    price: 129.95,
+    description:
+      "Iconic aviator sunglasses with UV-protected lenses and premium metal frame.",
+    category: "men's accessories",
+    image:
+      "https://assets2.glasses.com/cdn-record-files-pi/5671c48a-8b74-490e-bdf3-a35900054e90/9e05ca60-507a-4d9b-a9c6-ad1b00799a81/0RB3025__W0879__STD__shad__fr.png?impolicy=GL_parameters_transp_clone550",
+    rating: {
+      rate: 4.7,
+      count: 890,
+    },
+  },
+  {
+    id: 24,
+    title: "Dell Ultrasharp 24 Monitor",
+    price: 249.99,
+    description:
+      "Crystal-clear IPS display with ultra-thin bezels. Perfect for productivity.",
+    category: "electronics",
+    image:
+      "https://i.dell.com/is/image/DellContent//content/dam/images/products/electronics-and-accessories/dell/monitors/ultrasharp/u2421e/u2421e-xfp-03-gy.psd?fmt=pjpg&pscan=auto&scl=1&wid=4466&hei=2714&qlt=100,1&resMode=sharp2&size=4466,2714&chrss=full&imwidth=5000",
+    rating: {
+      rate: 4.5,
+      count: 270,
+    },
+  },
+  {
+    id: 25,
+    title: "Reebok Workout Sneakers",
+    price: 74.99,
+    description:
+      "Lightweight training shoes offering stability and comfort during exercise.",
+    category: "men's shoes",
+    image:
+      "https://www.sportspower.com.au/cdn/shop/files/Reebok_20Nano_20Gym_20Womens_20Training_20Shoes-100208626-undefined_web001_e649c6d9-1a3e-4c98-8096-e75e1b18b412.png?v=1753106381",
+    rating: {
+      rate: 4.1,
+      count: 145,
+    },
+  },
+  {
+    id: 26,
+    title: "North Face Puffer Jacket",
+    price: 189.99,
+    description:
+      "Insulated winter jacket designed to keep you warm in extreme cold.",
+    category: "women's clothing",
+    image:
+      "https://images.asos-media.com/products/the-north-face-retro-nuptse-polar-fleece-puffer-jacket-in-black/206576627-1-black?$n_640w$&wid=513&fit=constrain",
+    rating: {
+      rate: 5,
+      count: 520,
+    },
+  },
+  {
+    id: 27,
+    title: "Bose SoundLink Bluetooth Speaker",
+    price: 119.99,
+    description:
+      "Portable Bluetooth speaker with deep bass and long-lasting battery.",
+    category: "electronics",
+    image:
+      "https://alephksa.com/cdn/shop/files/cq5dam.web.1000.1000_7116cb98-1b80-4283-a164-0d8ea81008a8.png?v=1711630312",
+    rating: {
+      rate: 4.6,
+      count: 640,
+    },
+  },
+  {
+    id: 28,
+    title: "Levi's Slim Fit Jeans",
+    price: 49.99,
+    description: "Classic slim-fit jeans made from high-quality denim.",
+    category: "men's clothing",
+    image:
+      "https://www.dandyfellow.com/images/511-slim-fit-jeans-p35342-297050_zoom.jpg",
+    rating: {
+      rate: 4.3,
+      count: 380,
+    },
+  },
+  {
+    id: 29,
+    title: "Garmin Forerunner Sports Watch",
+    price: 179.99,
+    description:
+      "GPS-enabled sports watch for runners with advanced performance analytics.",
+    category: "electronics",
+    image:
+      "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1749146474-61uvcluxYlL.jpg?crop=0.854xw:0.854xh;0.0721xw,0.0737xh&resize=980:*",
+    rating: {
+      rate: 4.7,
+      count: 455,
+    },
+  },
+  {
+    id: 30,
+    title: "Columbia Hiking Backpack 35L",
+    price: 89.99,
+    description:
+      "Comfortable and durable 35-liter hiking backpack with waterproof cover.",
+    category: "men's accessories",
+    image:
+      "https://assets.trailspace.com/assets/0/2/5/9240613/F11_UU9865_491_f.jpg",
+    rating: {
+      rate: 4.4,
+      count: 200,
+    },
+  },
+];
